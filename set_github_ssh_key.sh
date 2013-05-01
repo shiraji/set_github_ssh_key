35213c85e6693c7e5955bf244b00ade6de09ea95
root <root@www7443ui.sakura.ne.jp>
2013-05-02 00:31:16 +0900

    update to not to have parameters

--- set_github_ssh_key.sh
+++ set_github_ssh_key.sh
@@ -1,43 +1,47 @@
 #!/bin/sh
-
-if [ $# -lt 2 ]; then
-	echo ""
-	echo "Usage: set_github_ssh_key.sh GITHUB_USER GITHUB_PASSWORD [SSH_KEY_PASSWORD]"
-	echo ""
-	exit 1
+ 
+#get GitHub user name if it is not provided
+if [ $# -lt 1 ]; then
+	echo -n "Enter GitHub user name: "
+	read _githubUsername
+else
+	_githubUsername=$1
 fi
-
+ 
+#get GitHub password
+echo -n "Enter GitHub password: "
+read -s _githubPassword
+echo
+ 
+#get SSH password
+echo -n "Enter SSH Key password: "
+read -s _sshKeyPassword
+echo
+ 
 #github settings
-_githubUsername=$1
-_githubPassword=$2
 _githubAuthURL="https://api.github.com/authorizations"
 _githubUserKeyURL="https://api.github.com/user/keys"
 _githubSSHKeyTitle=`hostname -s`
-
+ 
 #ssh key settings
 _sshKeyDir="$HOME/.ssh"
 _sshKeyFile="$_sshKeyDir/id_rsa"
 _sshKeyPubFile=$_sshKeyFile".pub"
-_sshKeyPassword=""
-if [ $# -ge 3 ]; then
-	_sshKeyPassword=$3
-fi
-
-#generate a token
-_token=`curl -u "${_githubUsername}:${_githubPassword}" -d "{\"scopes\":[\"user\"]}" $_githubAuthURL | grep "token" | cut -d"\"" -f4`
-
+ 
 #create ssh folder if it does not exist
 if [ ! -e $_sshKeyDir ]; then
 	mkdir -p $_sshKeyDir
 fi
-
+ 
 #run ssh-keygen if private key file does not exist
 if [ ! -e $_sshKeyFile ]; then
 	ssh-keygen -f $_sshKeyFile -N "$_sshKeyPassword"
 fi
-
+ 
+#generate a token
+_token=`curl -u "${_githubUsername}:${_githubPassword}" -d "{\"scopes\":[\"user\"]}" $_githubAuthURL | grep "token" | cut -d"\"" -f4`
+ 
 #post ssh public key
 curl -H "Authorization: token $_token" $_githubUserKeyURL -d "{ \"title\":\"$_githubSSHKeyTitle\", \"key\":\"`cat $_sshKeyPubFile`\" }"
-
+ 
 exit 0
-
