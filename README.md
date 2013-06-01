2851017933adadac6f89e170e8ee4e0fc20c361c
shiraji <isogai.shiraji@gmail.com>
2013-06-01 20:15:26 +0900

    create own github ssh key

--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 VPSなどで新しく環境作ったときに、GitHub APIのUser Public Keys APIを利用し、GitHubへ公開鍵を設定させる。
 
 ##使い方
-`set_github_ssh_key.sh GITHUB_USER GITHUB_PASSWORD [SSH_KEY_PASSWORD]`
+`set_github_ssh_key.sh GITHUB_USER`
 
 ###パラメータ
 * GITHUB_USER: 公開鍵を設定するGitHubユーザ名
-* GITHUB_PASSWORD：GITHUB_USERのパスワード
-* SSH_KEY_PASSWORD：公開鍵へつけるパスワード（デフォルトは空のパスワード）
 
 ##解説
 $HOME/.ssh/id_rsaにssh-keygenをに対して行うが、すでにファイルが存在していた場合、ssh-keygenを行わず、そのファイルで公開鍵の設定を行う。
--- set_github_ssh_key.sh
+++ set_github_ssh_key.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
- 
+
 #get GitHub user name if it is not provided
 if [ $# -lt 1 ]; then
 	echo -n "Enter GitHub user name: "
@@ -7,41 +7,67 @@ if [ $# -lt 1 ]; then
 else
 	_githubUsername=$1
 fi
- 
+
 #get GitHub password
 echo -n "Enter GitHub password: "
 read -s _githubPassword
 echo
- 
+
+#get GitHub SSH title. Default value is the result of `hostname -s`
+echo -n "Enter GitHub SSH Key Title [hostname -s]: "
+read _githubSSHKeyTitle
+_githubSSHKeyTitle=${_githubSSHKeyTitle:-`hostname -s`}
+
 #get SSH password
 echo -n "Enter SSH Key password: "
 read -s _sshKeyPassword
 echo
- 
+
 #github settings
 _githubAuthURL="https://api.github.com/authorizations"
 _githubUserKeyURL="https://api.github.com/user/keys"
-_githubSSHKeyTitle=`hostname -s`
- 
+
 #ssh key settings
-_sshKeyDir="$HOME/.ssh"
-_sshKeyFile="$_sshKeyDir/id_rsa"
-_sshKeyPubFile=$_sshKeyFile".pub"
- 
+_sshDir="$HOME/.ssh"
+_sshKeyGitHubDir="${_sshDir}/github"
+_sshKeyFile="${_sshKeyGitHubDir}/${_githubUsername}_rsa"
+_sshKeyPubFile="${_sshKeyFile}.pub"
+
 #create ssh folder if it does not exist
-if [ ! -e $_sshKeyDir ]; then
-	mkdir -p $_sshKeyDir
+if [ ! -e ${_sshKeyGitHubDir} ]; then
+	mkdir -p ${_sshKeyGitHubDir}
 fi
- 
+
+#put github settings to ssh config
+_sshConfigFile="${_sshDir}/config"
+if grep -Fxq "Host ${_githubUsername}.github.com" ${_sshConfigFile}
+then
+	echo "Github.com settings found: " $_sshConfigFile
+	echo "Following line should be added to ${_sshConfigFile}"
+	echo "Host ${_githubUsername}.github.com"
+	echo "	Hostname github.com"
+	echo "	PreferredAuthentications publickey"
+	echo "	IdentityFile ${_sshKeyFile}"
+else
+
+cat >> ${_sshConfigFile} << EOS
+Host ${_githubUsername}.github.com
+	Hostname github.com
+	PreferredAuthentications publickey
+	IdentityFile ${_sshKeyFile}
+EOS
+
+fi
+
 #run ssh-keygen if private key file does not exist
-if [ ! -e $_sshKeyFile ]; then
-	ssh-keygen -f $_sshKeyFile -N "$_sshKeyPassword"
+if [ ! -e ${_sshKeyFile} ]; then
+	ssh-keygen -f ${_sshKeyFile} -N "${_sshKeyPassword}"
 fi
- 
+
 #generate a token
-_token=`curl -u "${_githubUsername}:${_githubPassword}" -d "{\"scopes\":[\"user\"]}" $_githubAuthURL | grep "token" | cut -d"\"" -f4`
- 
+_token=`curl -u "${_githubUsername}:${_githubPassword}" -d "{\"scopes\":[\"user\"]}" ${_githubAuthURL} | grep "token" | cut -d"\"" -f4`
+
 #post ssh public key
-curl -H "Authorization: token $_token" $_githubUserKeyURL -d "{ \"title\":\"$_githubSSHKeyTitle\", \"key\":\"`cat $_sshKeyPubFile`\" }"
- 
+curl -H "Authorization: token ${_token}" ${_githubUserKeyURL} -d "{ \"title\":\"${_githubSSHKeyTitle}\", \"key\":\"`cat ${_sshKeyPubFile}`\" }"
+
 exit 0
